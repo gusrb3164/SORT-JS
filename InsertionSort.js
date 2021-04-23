d90b690c12affcbfb993d07fac2986f0bdcb6013
hyun gyu <gusrb3164@gmail.com>
2021-04-23 21:30:39 +0900

    fix InsertionSort

--- InsertionSort.js
+++ InsertionSort.js
@@ -1,9 +1,8 @@
 function InsertionSort(arr, compare) {
 	for (let end = 1; end < arr.length; end++) {
 		let idx = end;
-		while (idx > 0) {
-			if (compare(arr[idx - 1], arr[idx]) > 0)
-				[arr[idx - 1], arr[idx]] = [arr[idx], arr[idx - 1]];
+		while (idx > 0 && compare(arr[idx - 1], arr[idx]) > 0) {
+			[arr[idx - 1], arr[idx]] = [arr[idx], arr[idx - 1]];
 			idx--;
 		}
 	}
